53cf449e67cdc977859e168810d97be8a18c89e7
leoglej <leoglej@yahoo.com>
2017-02-03 17:25:31 -0800

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InventoryShoes.Startup))]
+namespace InventoryShoes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
